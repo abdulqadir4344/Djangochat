980a3a61cf494857bcf0bf60fa3eab03b3ba7f22
abdul <abdul>
2023-08-02 11:16:09 +0530

    02-08-2023

--- djangochat/core/views.py
+++ djangochat/core/views.py
@@ -14,7 +14,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()    
 
-            login(request,user)
+            login(request, user)
             
             return redirect('frontpage')
     else:
